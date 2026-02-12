e79e928b94ea8d9d09b849b64fb5873ead47eaf1
Ellen Sosa <elsbeth.ellen9@gmail.com>
2026-02-12 09:25:05 -0600

    agrego modificación suma

--- Unidad3/Suma.py
+++ Unidad3/Suma.py
@@ -1,7 +1,2 @@
-
-num1 = float(input("Ingrese el primer número: "))
-num2 = float(input("Ingrese el segundo número: "))
-
-suma = num1 + num2
-
-print("La suma es:", suma)
+def suma(x,y):
+    return x + y
